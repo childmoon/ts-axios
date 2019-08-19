46f0868ac168736d1c6e4998e286f6391058098a
cengyongpeng <940391955@qq.com>
2019-08-19 09:37:13 +0800

    processheader处理请求头

---
+++ src/helpers/headers.ts
@@ -0,0 +1,23 @@
+import {isPlainObject} from "./util";
+
+function normallizeHeaderName(headers:any,normalizedName:string):void {
+   if(!headers){
+     return
+   }
+   Object.keys(headers).forEach((name)=>{
+     if(name!==normalizedName&&name.toUpperCase()===
+     normalizedName.toUpperCase()){
+       headers[normalizedName]=headers[name]
+       delete headers[name]
+     }
+   })
+}
+
+export function processHeaders(headers:any,data:any):any {
+  normallizeHeaderName(headers,'Content-Type')
+  if(isPlainObject(data)){
+    if(headers && !headers['Content-Type']){
+      headers['Content-Type']='application/json;charset=utf-8'
+    }
+  }
+}
